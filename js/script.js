0ac38fa795c3a03be0cdfb50fb958e2511fb8d1e
Korny94 <karlmagnusnokling@hotmail.no>
2023-01-05 05:09:04 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,314 @@
-console.log ("This is script 1");
+// VIEW IN CONSOLE
+
+console.log("This is script 1");
+
+
+// ALERT (POP-UP MESSAGE)
+
+    // alert("Hello");
+
+    // window.alert("Wadup?");
+
+
+// VARIABLES 
+
+var firstName;
+
+firstName = "Isabel" + " ";
+
+// OR 
+
+var lastName = "Ivaylova Staneva";
+
+var whenWasIsabelBorn = "29.06.2001";
+
+var howOldIsIsabel = 21;
+
+var isIsabelMean = false;
+
+var isIsabelNice = true;
+
+var isIsabelALion = true;
+
+
+// OPERATORS
+
+/* 
+
+
+Operator    Description                 Example     Result
+
+===         equal to (type & value)	    3 === 3	    true ("3" === 3 FALSE) 
+
+==          equal to (only value)       "3" == 3    true
+
+!==	        not equal to (type & value) 3 !== 3	    false ("3" !== 3 TRUE)
+
+!=          not equal to (only value)   "3" != 2    true
+
+>	        greater than	            6 > 4	    true
+
+>=	        greater than or equal to	5 >= 4	    true
+
+<	        less than	                5 < 4	    false
+
+<=	        less than or equal to	    3 <= 4	    true
+
+
+
+*/
+
+console.log("is 7 equal === to 7?", 7 === 7); // Output: True
+
+console.log("is 7 equal === to 8?", 7 === 8); // Output: False
+
+console.log("is string 7 equal === to 7?", "7" === 7); // Output: False
+
+
+console.log("is string 7 equal == to 7?", "7" == 7); // Output: True
+
+console.log("is string 7 equal == to 8?", "7" == 8); // Output: False
+
+
+console.log("is 7 not equal !== to 7?", 7 !== 7); // Output: False
+
+console.log("is 7 not equal !== to 8?", 7 !== 8); // Output: True
+
+console.log("is string 7 not equal !== to 7?", "7" !== 7); // Output: True
+
+
+console.log("is 7 not equal != to 8?", 7 != 8); // Output: True
+
+console.log("is 7 not equal != to 7?", 7 != 7); // Output: False
+
+console.log("is string 7 not equal != to 7?", "7" != 7); // Output: False
+
+
+console.log("is 7 greater than 6?", 7 > 6); // Output: True
+ 
+console.log("is 7 greater than 8?", 7 > 8); // Output: False
+
+
+console.log("is 7 greater than or equal to 7?", 7 >= 7); // Output: True
+
+console.log("is 7 greater than or equal to 6?", 7 >= 6); // Output: True
+
+console.log("is 7 greater than or equal to 8?", 7 >= 8); // Output: False
+
+
+console.log("is 7 less than 8?", 7 < 8); // Output: True
+
+console.log("is 7 less than 6?", 7 < 6); // Output: False
+
+
+console.log("is 7 less than or equal to 7?", 7 <= 7); // Output: True
+
+console.log("is 7 less than or equal to 6?", 7 <= 6); // Output: False
+
+console.log("is 7 less than or equal to 8?", 7 <= 8); // Output: True
+
+var myNumber = 7;
+var myString = "dog";
+
+// is myNumber greater than 8?
+console.log("is myNumber 7 greater than 8?", myNumber > 8) // Output: False
+
+// is myNumber less than or equal to 8?
+console.log("is myNumber 7 less than or equal to 8?", myNumber <= 8) // Output: True
+
+// is myString exactly equal to "dog"?
+console.log("is myString dog exactly equal to dog?", myString === "dog") // Output: True
+
+// is myString not equal to "cat"?
+console.log("is myString dog not equal to cat?", myString !== "cat") // Output: True
+
+
+
+
+
+// JOINING STRINGS TOGETHER
+
+var fullName = firstName + lastName;
+
+var fullName2 = "Isabel" + " " + "Ivaylova Staneva";
+
+console.log(fullName);
+
+console.log(fullName + " " + fullName2);
+
+
+var like1 = "passion";
+
+var like2 = "romance";
+
+var like3 = "honesty";
+
+var like4 = "loyalty";
+
+var comma = ",";
+
+var and = "and";
+
+console.log(fullName, "likes", like1 + comma, like2 + comma, like3, and, like4);
+
+
+var number1 = "10";
+
+var number2 = "5";
+
+var joinedNumber = number1 + number2;
+
+console.log("If you put", number1, "and", number2, "together, you get", joinedNumber);
+
+
+// LENGTH OF STRING (HOW MANY CHARACTERS)
+
+var lengthOfString = fullName.length;
+
+console.log("There are", lengthOfString, "characters in Isabel Ivaylova Staneva including spaces");
+
+
+// ADDING NUMBERS
+
+var blueCars = 3;
+
+var redCars = 8;
+
+var totalCars = blueCars + redCars;
+
+console.log( blueCars, "blue cars plus", redCars, "red cars is", totalCars, "total cars");
+
+
+// SUBTRACTING NUMBERS
+
+var blueCars = 3;
+
+var redCars = 8;
+
+var totalCars2 = blueCars - redCars;
+
+console.log(blueCars, "blue cars minus", redCars, "red cars are", totalCars2, "total cars");
+
+
+// DIVIDING NUMBERS
+
+var blueCars = 10;
+
+var redCars = 4;
+
+var totalCars3 = blueCars / redCars;
+
+console.log(blueCars, "blue cars divided by", redCars, "red cars are", totalCars3, "total cars");
+
+
+// MULTIPLYING NUMBERS
+
+var blueCars = 10;
+
+var redCars = 4;
+
+var totalCars4 = blueCars * redCars;
+
+console.log(blueCars, "blue cars times", redCars, "red cars is", totalCars4, "total cars");
+
+
+// REMAINDER NUMBERS
+
+var thirteen = 13;
+
+var five = 5;
+
+var remainder = thirteen % five;
+
+console.log("the remainder of 13 divided by 5 is", remainder);
+
+
+// CONVERT NUMBERED STRING TO ACTUAL NUMBER
+
+    // INTEGER
+
+    var integer = "7";
+
+    var convertedInteger = parseInt(integer);
+
+    // DECIMAL
+
+    var decimal = "7.9";
+
+    var convertedDecimal = parseFloat(decimal);
+
+    var totalNum = convertedInteger + convertedDecimal;
+
+    console.log("7 plus 7.9 is", totalNum);
+
+    // SIMPLIFIED
+
+    var result = parseFloat("10.2") + parseInt("4");
+
+    console.log("10.2 plus 4 is", result);
+
+
+// NaN (Not a Number)
+
+var number1 = 23;
+
+var number2 = "frfr";
+
+console.log("23 minus frfr is", number1 - number2);
+
+    // Check if it is a number isNaN (true = not a number, false = a number)
+
+    var isNumber = isNaN(number1 - number2);
+
+    console.log("is 23 minus frfr not a number?", isNumber);
+
+
+// TYPEOF OPERATOR
+
+var number = 5;
+
+console.log("number is a", typeof number);
+
+var string = "Hello";
+
+console.log("string is a", typeof string);
+
+var boolean = true;
+
+var booleanTypeOf = typeof boolean;
+
+console.log("boolean is a", booleanTypeOf);
+
+
+
+// SELECT & MODIFY HTML ELEMENTS
+
+var heading1 = document.querySelector("h1");
+
+heading1.style.color = "blue";
+
+
+var spanH1 = document.querySelector("span");
+
+spanH1.style.borderBottom = "5px dashed red"
+
+
+var dummyText = document.querySelector(".dummyText");
+
+dummyText.style.fontSize = "2rem";
+
+
+var dummyText = document.querySelector("#dummyText2");
+
+dummyText2.style.background = "red";
+
+// SHORTER - SELECT & MODIFY HTML ELEMENTS
+
+var dummyText = document.querySelector(".dummyText3").style.color = "blue";
+
+
+// CHANGE/ADD/REMOVE HTML CONTENT
+
+var heading2 = document.querySelector("h2");
+
+heading2.innerHTML = "Wadup";
--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,128 @@
-console.log ("This is script 2");
+console.log ("This is script 2 - LESSON TASKS");
+
+/* Lesson Task 2 Questions
+Add all the answers to the script.js file.
+
+Giving your variables meaningful names is very important. The names should indicate what type of information or data the variable holds.
+
+Question 1
+Create two number variables, one with an integer value and one with a decimal value. */
+
+var numberOne = 10;
+
+var numberTwo = 10.5;
+
+/* Question 2
+Declare and initialise a variable with all the working days of the week separated by hyphens -. */
+
+var weekdays = "Monday - Tuesday - Wednesday - Thursday - Friday - Saturday - Sunday";
+
+console.log("the days of the week are:", weekdays)
+
+var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+
+console.log(daysOfTheWeek);
+
+/* Question 3
+Create four variables that demonstrate the following operations:
+
+addition
+subtraction
+multiplication
+division */
+
+var addition = 4 + 6;
+var subtraction = 8 - 6;
+var multiplication = 10 * 10;
+var division = 20 / 7;
+
+console.log("10 plus 10.5 is", numberOne + numberTwo);
+
+console.log("10 minus 10.5 is", numberOne - numberTwo);
+
+console.log("10 times 10.5 is", numberOne * numberTwo);
+
+console.log("10 divided 10.5 is", numberOne / numberTwo);
+
+/* Question 4
+Declare a boolean variable with an initial value of false. */
+
+var haleyIsTheBest = true;
+
+console.log("Is Haley the best?", haleyIsTheBest);
+
+/* Question 5
+Console log the number of characters in this string: "The frogs are angry." */
+
+var sentence = "The frogs are angry.";
+
+console.log("How many characters including spaces are in: The frogs are angry. =", sentence.length)
+
+/* Question 6
+Convert the following variable names to camelCase:
+
+var time_elapsed;   = timeElapsed
+var totalprice;     = totalPrice
+
+/* Question 7
+Convert the following string values to number values: */
+
+var number1 = "53";
+
+var number2 = "44.6";
+
+var number1 = parseInt(number1);
+
+var number2 = parseFloat(number2);
+
+console.log(number1 + number2);
+
+/* Question 8
+Check the type of the following variables and log the results: */
+
+var pet = "donkey";
+
+var age = 89;
+
+var discountApplied = true;
+
+var timeLeft;
+
+var typeOfPet = typeof pet;
+
+console.log("pet is a", typeOfPet);
+
+var typeOfAge = typeof age;
+
+console.log("age is a", typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+
+console.log("discountApplied is a", typeOfDiscountApplied);
+
+var typeOfTimeLeft = typeof timeLeft;
+
+console.log("timeLeft is", typeOfTimeLeft);
+
+
+/* Question 9
+Declare and initialise a variable with the value "Updating heading".
+
+Select the h1 element on the page and set it's inner HTML value to be the variable created above. */
+
+var heading = document.querySelector("h1");
+
+heading.innerHTML = "Updating heading";
+
+var headingText = "Updating heading";
+
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+/* Question 10
+Select the body element on the page and change its background colour to lightskyblue. */
+
+var body = document.querySelector("body").style.backgroundColor = "lightskyblue";
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
