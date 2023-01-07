5078ba5439f98ce7d2d6e9929c7b8a9dd94a2b21
Korny94 <karlmagnusnokling@hotmail.no>
2023-01-07 03:35:44 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-// VIEW IN CONSOLE
+    // VIEW IN CONSOLE
 
-console.log("This is script 1");
-
-
-// ALERT (POP-UP MESSAGE)
-
-    // alert("Hello");
-
-    // window.alert("Wadup?");
+console.log("This is script 1"); 
+// Output: This is script 1
 
 
-// VARIABLES 
+    // ALERT (POP-UP MESSAGE)
+
+// alert("Hello");      
+// Output: Hello
+
+// window.alert("Wadup?");  
+// Output: Wadup
+
+
+    // COMMA VS PLUS
+
+console.log(1 + 2);     // Output: 3
+
+console.log(1, 2);      // Output: 1 2
+
+
+    // VARIABLES var
 
 var firstName;
 
-firstName = "Isabel" + " ";
+firstName = "Isabel" + " ";     
+// Output: Isabel 
 
 // OR 
 
@@ -31,7 +42,7 @@ var isIsabelNice = true;
 var isIsabelALion = true;
 
 
-// OPERATORS
+    // OPERATORS
 
 /* 
 
@@ -54,85 +65,128 @@ Operator    Description                 Example     Result
 
 <=	        less than or equal to	    3 <= 4	    true
 
++           addition                    3 + 4       7
+
+-           subtraction                 4 - 3       1
+
+*           multiplication              3 * 4       12
+
+/           division                    4 / 2       2
+
+%           remainder                   5 % 2       1
+
+**          expoentiation               2 ** 3      8
+
+++          increment                   ++2         3
+
+--          decrement                   --2         1
+
 
 
 */
 
-console.log("is 7 equal === to 7?", 7 === 7); // Output: True
+console.log("is 7 equal === to 7?", 7 === 7); 
+// Output: True
 
-console.log("is 7 equal === to 8?", 7 === 8); // Output: False
+console.log("is 7 equal === to 8?", 7 === 8); 
+// Output: False
 
-console.log("is string 7 equal === to 7?", "7" === 7); // Output: False
-
-
-console.log("is string 7 equal == to 7?", "7" == 7); // Output: True
-
-console.log("is string 7 equal == to 8?", "7" == 8); // Output: False
+console.log("is string 7 equal === to 7?", "7" === 7); 
+// Output: False
 
 
-console.log("is 7 not equal !== to 7?", 7 !== 7); // Output: False
+console.log("is string 7 equal == to 7?", "7" == 7); 
+// Output: True
 
-console.log("is 7 not equal !== to 8?", 7 !== 8); // Output: True
-
-console.log("is string 7 not equal !== to 7?", "7" !== 7); // Output: True
-
-
-console.log("is 7 not equal != to 8?", 7 != 8); // Output: True
-
-console.log("is 7 not equal != to 7?", 7 != 7); // Output: False
-
-console.log("is string 7 not equal != to 7?", "7" != 7); // Output: False
+console.log("is string 7 equal == to 8?", "7" == 8); 
+// Output: False
 
 
-console.log("is 7 greater than 6?", 7 > 6); // Output: True
- 
-console.log("is 7 greater than 8?", 7 > 8); // Output: False
+console.log("is 7 not equal !== to 7?", 7 !== 7); 
+// Output: False
+
+console.log("is 7 not equal !== to 8?", 7 !== 8); 
+// Output: True
+
+console.log("is string 7 not equal !== to 7?", "7" !== 7); 
+// Output: True
 
 
-console.log("is 7 greater than or equal to 7?", 7 >= 7); // Output: True
+console.log("is 7 not equal != to 8?", 7 != 8); 
+// Output: True
 
-console.log("is 7 greater than or equal to 6?", 7 >= 6); // Output: True
+console.log("is 7 not equal != to 7?", 7 != 7); 
+// Output: False
 
-console.log("is 7 greater than or equal to 8?", 7 >= 8); // Output: False
-
-
-console.log("is 7 less than 8?", 7 < 8); // Output: True
-
-console.log("is 7 less than 6?", 7 < 6); // Output: False
+console.log("is string 7 not equal != to 7?", "7" != 7); 
+// Output: False
 
 
-console.log("is 7 less than or equal to 7?", 7 <= 7); // Output: True
+console.log("is 7 greater than 6?", 7 > 6); 
+// Output: True
 
-console.log("is 7 less than or equal to 6?", 7 <= 6); // Output: False
+console.log("is 7 greater than 8?", 7 > 8); 
+// Output: False
 
-console.log("is 7 less than or equal to 8?", 7 <= 8); // Output: True
+
+console.log("is 7 greater than or equal to 7?", 7 >= 7); 
+// Output: True
+
+console.log("is 7 greater than or equal to 6?", 7 >= 6); 
+// Output: True
+
+console.log("is 7 greater than or equal to 8?", 7 >= 8); 
+// Output: False
+
+
+console.log("is 7 less than 8?", 7 < 8); 
+// Output: True
+
+console.log("is 7 less than 6?", 7 < 6); 
+// Output: False
+
+
+console.log("is 7 less than or equal to 7?", 7 <= 7); 
+// Output: True
+
+console.log("is 7 less than or equal to 6?", 7 <= 6); 
+// Output: False
+
+console.log("is 7 less than or equal to 8?", 7 <= 8); 
+// Output: True
 
 var myNumber = 7;
 var myString = "dog";
 
 // is myNumber greater than 8?
-console.log("is myNumber 7 greater than 8?", myNumber > 8) // Output: False
+console.log("is myNumber 7 greater than 8?", myNumber > 8) 
+// Output: False
 
 // is myNumber less than or equal to 8?
-console.log("is myNumber 7 less than or equal to 8?", myNumber <= 8) // Output: True
+console.log("is myNumber 7 less than or equal to 8?", myNumber <= 8) 
+// Output: True
 
 // is myString exactly equal to "dog"?
-console.log("is myString dog exactly equal to dog?", myString === "dog") // Output: True
+console.log("is myString dog exactly equal to dog?", myString === "dog") 
+// Output: True
 
 // is myString not equal to "cat"?
-console.log("is myString dog not equal to cat?", myString !== "cat") // Output: True
+console.log("is myString dog not equal to cat?", myString !== "cat") 
+// Output: True
 
 
 
-// JOINING STRINGS TOGETHER
+    // JOINING STRINGS TOGETHER +
 
 var fullName = firstName + lastName;
 
 var fullName2 = "Isabel" + " " + "Ivaylova Staneva";
 
-console.log(fullName);
+console.log(fullName);      
+// Output: Isabel Ivaylova Staneva
 
-console.log(fullName + " " + fullName2);
+console.log(fullName + " " + fullName2); 
+// Output: Isabel Ivaylova Staneva Isabel Ivaylova Staneva
 
 
 var like1 = "passion";
@@ -147,7 +201,8 @@ var comma = ",";
 
 var and = "and";
 
-console.log(fullName, "likes", like1 + comma, like2 + comma, like3, and, like4);
+console.log(fullName, "likes", like1 + comma, like2 + comma, like3, and, like4);   
+// Output: Isabel Ivaylova Staneva likes passion, romance, honesty and loyalty 
 
 
 var number1 = "10";
@@ -156,17 +211,19 @@ var number2 = "5";
 
 var joinedNumber = number1 + number2;
 
-console.log("If you put", number1, "and", number2, "together, you get", joinedNumber);
+console.log("If you put", number1, "and", number2, "together, you get", joinedNumber);      
+// Output: If you put 10 and 5 together, you get 105
 
 
-// LENGTH OF STRING (HOW MANY CHARACTERS)
+    // LENGTH OF STRING (HOW MANY CHARACTERS) varName.length
 
 var lengthOfString = fullName.length;
 
-console.log("There are", lengthOfString, "characters in Isabel Ivaylova Staneva including spaces");
+console.log("There are", lengthOfString, "characters in Isabel Ivaylova Staneva including spaces");     
+// Output: There are 23 characters in Isabel Ivaylova Staneva including spaces
 
 
-// ADDING NUMBERS
+    // ADDING NUMBERS +
 
 var blueCars = 3;
 
@@ -174,10 +231,11 @@ var redCars = 8;
 
 var totalCars = blueCars + redCars;
 
-console.log( blueCars, "blue cars plus", redCars, "red cars is", totalCars, "total cars");
+console.log(blueCars, "blue cars plus", redCars, "red cars is", totalCars, "total cars");
+// Output: 3 'blue cars plus' 8 'red cars is' 11 'total cars'
 
 
-// SUBTRACTING NUMBERS
+    // SUBTRACTING NUMBERS -
 
 var blueCars = 3;
 
@@ -186,9 +244,10 @@ var redCars = 8;
 var totalCars2 = blueCars - redCars;
 
 console.log(blueCars, "blue cars minus", redCars, "red cars are", totalCars2, "total cars");
+// Output: 3 'blue cars minus' 8 'red cars are' -5 'total cars'
 
 
-// DIVIDING NUMBERS
+    // DIVIDING NUMBERS /
 
 var blueCars = 10;
 
@@ -197,9 +256,10 @@ var redCars = 4;
 var totalCars3 = blueCars / redCars;
 
 console.log(blueCars, "blue cars divided by", redCars, "red cars are", totalCars3, "total cars");
+// Output: 10 'blue cars divided by' 4 'red cars are' 2.5 'total cars'
 
 
-// MULTIPLYING NUMBERS
+    // MULTIPLYING NUMBERS *
 
 var blueCars = 10;
 
@@ -207,10 +267,11 @@ var redCars = 4;
 
 var totalCars4 = blueCars * redCars;
 
-console.log(blueCars, "blue cars times", redCars, "red cars is", totalCars4, "total cars");
+console.log(blueCars + " " + "blue cars times" + redCars + "red cars is" + totalCars4 + "total cars");
+// Output: 10 'blue cars times' 4 'red cars is' 40 'total cars'
 
 
-// REMAINDER NUMBERS
+    // REMAINDER NUMBERS %
 
 var thirteen = 13;
 
@@ -219,67 +280,143 @@ var five = 5;
 var remainder = thirteen % five;
 
 console.log("the remainder of 13 divided by 5 is", remainder);
+// Output: the remainder of 13 divided by 5 is 3
 
 
-// CONVERT NUMBERED STRING TO ACTUAL NUMBER
+    // EXPONENTIATION NUMBERS ** 3^4 (3*3*3*3)
 
-    // INTEGER
+var three = 3;
 
-    var integer = "7";
+var four = 4; 
 
-    var convertedInteger = parseInt(integer);
+var expo = three ** four;
 
-    // DECIMAL
-
-    var decimal = "7.9";
-
-    var convertedDecimal = parseFloat(decimal);
-
-    var totalNum = convertedInteger + convertedDecimal;
-
-    console.log("7 plus 7.9 is", totalNum);
-
-    // SIMPLIFIED
-
-    var result = parseFloat("10.2") + parseInt("4");
-
-    console.log("10.2 plus 4 is", result);
+console.log("3 to the power of 4 (3^4) is ", expo);
+// Output: 3 to the power of 4 (3^4) is  81
 
 
-// NaN (Not a Number)
+    // INCREMENT NUMBERS ++
+
+var increment = ++three;
+
+console.log("three incremented by one is", increment);
+// Output: three incremented by one is 4
+
+
+    // DECREMENT NUMBERS --
+
+var five = 5;
+
+var decrement = --five;
+
+console.log("five decremented by one is", decrement);
+// Output: five decremented by one is 4
+
+
+    // CONVERT NUMBERED STRING TO ACTUAL NUMBER parseInt for integer & parseFloat for decimal
+
+// INTEGER
+
+var integer = "7";
+
+var convertedInteger = parseInt(integer);
+
+// DECIMAL
+
+var decimal = "7.9";
+
+var convertedDecimal = parseFloat(decimal);
+
+var totalNum = convertedInteger + convertedDecimal;
+
+console.log("7 plus 7.9 is", totalNum);
+// Output: 7 plus 7.9 is 14.9
+
+// SIMPLIFIED
+
+var result = parseFloat("10.2") + parseInt("4");
+
+console.log("10.2 plus 4 is", result);
+// Output: 10.2 plus 4 is 14.2
+
+
+    // NaN (Not a Number)
 
 var number1 = 23;
 
 var number2 = "frfr";
 
 console.log("23 minus frfr is", number1 - number2);
+// Output: 23 minus frfr is NaN
 
     // Check if it is a number isNaN (true = not a number, false = a number)
 
-    var isNumber = isNaN(number1 - number2);
+var isNumber = isNaN(number1 - number2);
 
-    console.log("is 23 minus frfr not a number?", isNumber);
+console.log("is 23 minus frfr not a number?", isNumber);
+// Output: is 23 minus frfr not a number? true
 
 
-// TYPEOF OPERATOR
+    // TYPEOF OPERATOR typeof
 
 var number = 5;
 
 console.log("number is a", typeof number);
+// Output: number is a number
 
 var string = "Hello";
 
 console.log("string is a", typeof string);
+// Output: string is a string
 
 var boolean = true;
 
 var booleanTypeOf = typeof boolean;
 
 console.log("boolean is a", booleanTypeOf);
+// Output: boolean is a boolean
 
 
+    // "IF" & "ELSE" CONDITIONAL STATEMENTS
 
-// SELECT & MODIFY HTML ELEMENTS
+var isLoggedIn = true;
+
+if (isLoggedIn === true) {
+    console.log("The user is logged in");
+} else {
+    console.log("The user is logged out");
+}
+// Output: The user is logged in
+
+
+var isLoggedIn = false;
+
+if (isLoggedIn === true) {
+    console.log("The user is logged in");
+} else {
+    console.log("The user is logged out");
+}
+// Output: The user is logged out
+
+
+var inputIsValid = false;
+
+if (inputIsValid === false) {
+  // show error message
+} else {
+  // hide error message
+}
+
+
+var total = 80;
+
+if (total > 60) {
+    console.log("This is too expensive")
+} else {
+    console.log("This is affordable")
+}
+
+    // SELECT & MODIFY HTML ELEMENTS document.querySelector("")
 
 var heading1 = document.querySelector("h1");
 
@@ -305,8 +442,9 @@ dummyText2.style.background = "red";
 var dummyText = document.querySelector(".dummyText3").style.color = "blue";
 
 
-// CHANGE/ADD/REMOVE HTML CONTENT
+    // CHANGE/ADD/REMOVE HTML CONTENT varName.innerHTML
 
 var heading2 = document.querySelector("h2");
 
 heading2.innerHTML = "Wadup";
+
--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,5 @@
 console.log ("This is script 2 - LESSON TASKS");
+// Output: This is script 2 - LESSON TASKS
 
 /* Lesson Task 2 Questions
 Add all the answers to the script.js file.
@@ -18,10 +19,12 @@ Declare and initialise a variable with all the working days of the week separate
 var weekdays = "Monday - Tuesday - Wednesday - Thursday - Friday - Saturday - Sunday";
 
 console.log("the days of the week are:", weekdays)
+// Output: the days of the week are: Monday - Tuesday - Wednesday - Thursday - Friday - Saturday - Sunday
 
 var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
 
 console.log(daysOfTheWeek);
+// Output: Monday-Tuesday-Wednesday-Thursday-Friday
 
 /* Question 3
 Create four variables that demonstrate the following operations:
@@ -37,12 +40,16 @@ var multiplication = 10 * 10;
 var division = 20 / 7;
 
 console.log("10 plus 10.5 is", numberOne + numberTwo);
+// Output: 10 plus 10.5 is 20.5
 
 console.log("10 minus 10.5 is", numberOne - numberTwo);
+// Output: 10 minus 10.5 is -0.5
 
 console.log("10 times 10.5 is", numberOne * numberTwo);
+// Output: 10 times 10.5 is 105
 
 console.log("10 divided 10.5 is", numberOne / numberTwo);
+// Output: 10 divided 10.5 is 0.9523809523809523
 
 /* Question 4
 Declare a boolean variable with an initial value of false. */
@@ -50,13 +57,16 @@ Declare a boolean variable with an initial value of false. */
 var haleyIsTheBest = true;
 
 console.log("Is Haley the best?", haleyIsTheBest);
+// Output: Is Haley the best? true
 
 /* Question 5
 Console log the number of characters in this string: "The frogs are angry." */
 
 var sentence = "The frogs are angry.";
 
-console.log("How many characters including spaces are in: The frogs are angry. =", sentence.length)
+console.log("How many characters including spaces are in: The frogs are angry. =", sentence.length);
+// Output: How many characters including spaces are in: The frogs are angry. = 20
+
 
 /* Question 6
 Convert the following variable names to camelCase:
@@ -75,7 +85,8 @@ var number1 = parseInt(number1);
 
 var number2 = parseFloat(number2);
 
-console.log(number1 + number2);
+console.log("53 plus 44.6 is", number1 + number2);
+// Output: 53 plus 44.6 is 97.6
 
 /* Question 8
 Check the type of the following variables and log the results: */
@@ -91,18 +102,22 @@ var timeLeft;
 var typeOfPet = typeof pet;
 
 console.log("pet is a", typeOfPet);
+// Output: pet is a string
 
 var typeOfAge = typeof age;
 
 console.log("age is a", typeOfAge);
+// Output: age is a number
 
 var typeOfDiscountApplied = typeof discountApplied;
 
 console.log("discountApplied is a", typeOfDiscountApplied);
+// Output: discountApplied is a boolean
 
 var typeOfTimeLeft = typeof timeLeft;
 
 console.log("timeLeft is", typeOfTimeLeft);
+// Output: timeLeft is undefined
 
 
 /* Question 9
