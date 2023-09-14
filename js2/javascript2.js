0182a9668bef781036483bd3ea8c2aa29e06fa6c
Korny94 <karlmagnusnokling@hotmail.no>
2023-09-14 13:59:47 +0200

    update

--- js2/javascript2.js
+++ js2/javascript2.js
@@ -385,3 +385,349 @@ class Person {
 const newPerson = new Person("Ola Nordmann");
 
 console.log(newPerson.name);
+
+/* ------------------------------------------------------------------------------ */
+
+/* ARRAY METHODS */
+
+// .map() - Creates a new array with the results of calling a function for every array element
+
+// Original array
+const arrayValues = [5, 7, 9, 14];
+
+// New array called 'newArray' that contains the result from the `.map` method
+const newArray = arrayValues.map(function (value, index, array) {
+  return value * 2;
+});
+
+console.log(arrayValues, newArray);
+// Logs:
+// [5, 7, 9, 14] [10, 14, 18, 28]
+
+//--------//
+
+// .filter() - Creates a new array with array elements that passes a test
+
+// Original array
+
+const values = [100, 200, 300, 400, 500];
+
+const newArray2 = values.filter((element, index, array) => {
+  // Return true if you want to keep the item in the array being returned
+  // Return false if you don't want the item to be included in the array being returned
+  return element > 200;
+});
+
+console.log(newArray2); // Logs: [300, 400, 500] because 100 and 200 are not greater than 200
+
+//--------//
+
+// .reduce() - Reduces the array to a single value
+
+// previousValue = the value of the previous item in the array
+
+// Sum of these values is 25 (5 + 5 + 5 + 10)
+const values2 = [5, 5, 5, 10];
+
+const sumOfValues = values2.reduce((total, value) => {
+  // Add the current value to our total
+  total += value;
+  // Return the total
+  return total;
+});
+
+console.log(sumOfValues);
+// Returns:
+// 25
+
+//--------//
+
+// All of the in-stock products added together equals 170.95
+const products = [
+  { title: "Cheese", inStock: false, quantity: 2, price: 15.0 },
+  { title: "Milk", inStock: true, quantity: 5, price: 8.99 },
+  { title: "Bread", inStock: true, quantity: 3, price: 22.0 },
+  { title: "Egg", inStock: true, quantity: 12, price: 5 },
+];
+
+const totalCost = products.reduce(
+  (total, currentProduct) =>
+    currentProduct.inStock
+      ? (total += currentProduct.price * currentProduct.quantity)
+      : total,
+  0 // Initial value, 0 for number, "" for string, [] for array, {} for object
+);
+
+console.log(totalCost);
+// Returns:
+// 170.95
+
+//--------//
+
+// .find() - Returns the value of the first element in an array that passes a test
+// .findIndex() - Returns the index of the first element in an array that passes a test
+
+const values3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const foundValue = values3.find((currentValue, index) => {
+  console.log("Current index: ", index);
+  if (currentValue === 3) {
+    return true;
+  }
+});
+
+console.log("Found value:", foundValue);
+// The console logs the following:
+// Current index:  0
+// Current index:  1
+// Current index:  2
+// Current index:  3
+// Found value: 3
+
+//--------//
+
+const users2 = [
+  { name: "Kari", id: 19313 },
+  { name: "Hans", id: 40202 },
+  { name: "Joakim", id: 59230 },
+  { name: "Inger", id: 14023 },
+  { name: "Ellen", id: 76339 },
+];
+
+const foundUser = users2.find(({ name }) => name[0].toLowerCase() === "j");
+
+console.log(foundUser);
+// Logs: { name: 'Joakim', id: 59230 }
+
+//--------//
+
+const books = [
+  { title: "Building a Spaceship", inStock: false },
+  { title: "Growing Orchids", inStock: false },
+  { title: "River Fisher", inStock: false },
+];
+
+// Destructured the props and moved logic to the return
+const foundBook = books.find(({ inStock }) => inStock);
+
+if (!foundBook) {
+  console.log("No books in stock!");
+} else {
+  console.log("First book in stock:", foundBook);
+}
+// Returns:
+// No books in stock!
+
+//--------//
+
+// .some() - Checks if any of the elements in an array passes a test
+// .every() - Checks if all of the elements in an array passes a test
+
+const values4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const didMatchCondition = values4.some((value, index) => {
+  console.log({ index, value });
+  if (value >= 3) {
+    return true;
+  }
+});
+
+console.log("didMatchCondition:", didMatchCondition);
+// Return/output:
+// index: 0 value: 0
+// index: 1 value: 1
+// index: 2 value: 2
+// index: 3 value: 3
+// didMatchCondition: true
+
+//--------//
+
+const values5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const didAllMatchCondition = values5.every((value, index) => {
+  console.log("index:", index, "value:", value);
+  if (value >= 0) {
+    return true;
+  }
+});
+
+console.log("didAllMatchCondition:", didAllMatchCondition);
+// index: 0 value: 0
+// index: 1 value: 1
+// index: 2 value: 2
+// index: 3 value: 3
+// index: 4 value: 4
+// index: 5 value: 5
+// index: 6 value: 6
+// index: 7 value: 7
+// index: 8 value: 8
+// index: 9 value: 9
+// didAllMatchCondition: true
+
+//--------//
+
+// CHAINING ARRAY METHODS
+
+const values6 = [0, 1, 2, 3, 4];
+
+const result1 = values6
+  .filter(/* This filter first runs */)
+  .reduce(/* This reduce acts on the result of the filter */)
+  .map(/* This map acts on the result of the reduce above */);
+
+/* ------------------------------------------------------------------------------ */
+
+/* OBJECT METHODS */
+
+// An object is a collection of properties, and a property is an association between a name (or key) and a value.
+
+const person5 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+// To access a value from the object, we use a key. There are two ways we can use the key to access our values:
+// Dot notation
+//Bracket notation
+
+const firstName3 = person5.firstName;
+
+console.log(firstName3);
+// Returns:
+// Ola
+
+// OR
+
+const firstName4 = person5["firstName"];
+
+console.log(firstName4);
+// Returns:
+// Ola
+
+const recipe = {
+  ingredient_0: "Egg",
+  ingredient_1: "Milk",
+  ingredient_2: "Flour",
+};
+
+const ingredient0 = recipe["ingredient_" + 0];
+const ingredient1 = recipe["ingredient_" + 1];
+const ingredient2 = recipe["ingredient_" + 2];
+
+console.log(ingredient0);
+console.log(ingredient1);
+console.log(ingredient2);
+
+// Returns:
+// Egg
+// Milk
+// Flour
+
+//--------//
+
+// for...in loop - Loops through the properties of an object
+
+const person6 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+for (const key in person6) {
+  // Use the key to access the value from the key/value pair
+  console.log("key:", key, "value:", person6[key]);
+}
+// Returns:
+// key: firstName value: Ola
+// key: lastName value: Nordmann
+// key: module value: JavaScript
+
+//--------//
+
+// Object.keys() - Returns an array containing all of the keys that are in that object.
+
+const person7 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+const keysArray = Object.keys(person7);
+
+console.log(keysArray);
+// Returns:
+// ['firstName', 'lastName', 'module']
+
+//--------//
+
+// forEach()
+
+const person8 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+Object.keys(person8).forEach((key) => {
+  console.log(`key: ${key}, value: ${person8[key]}`);
+});
+
+// Logs:
+// key: firstName, value: Ola
+// key: lastName, value: Nordmann
+// key: module, value: JavaScript
+
+//--------//
+
+// map()
+
+const person9 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+// .map will return a new array that contains the same number
+// of elements as there were in the initial array
+const keysAndValues = Object.keys(person9).map((key) => {
+  return `key: ${key}, value: ${person9[key]}`;
+});
+
+console.log(keysAndValues);
+// Logs:
+// 0: "key: firstName, value: Ola"
+// 1: "key: lastName, value: Nordmann"
+// 2: "key: module, value: JavaScript"
+
+//--------//
+
+// Object.values() - Returns an array containing all of the values that are in that object.
+
+const person10 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+console.log(Object.values(person10));
+// Returns:
+// ['Ola', 'Nordmann', 'JavaScript']
+
+//--------//
+
+// Object.entries() - Returns an array containing all of the keys and values that are in that object.
+
+const person11 = {
+  firstName: "Ola",
+  lastName: "Nordmann",
+  module: "JavaScript",
+};
+
+Object.entries(person11).forEach((keyValuePair) => {
+  console.log(keyValuePair);
+});
+// Returns:
+// ['firstName', 'Ola']
+// ['lastName', 'Nordmann']
+// ['module', 'JavaScript']
